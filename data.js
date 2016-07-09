bb590b838ed044580ce2d17fe874345640265141
Andrey Popovich <and.popovich@gmail.com>
2016-07-09 23:26:18 +0300

    Add badges

--- data.js
+++ data.js
@@ -1,7 +1,19 @@
 module.exports = {
   'apopovich': {
     name: 'Андрей Попович',
-    badges: []
+    badges: [
+      {
+        color: 'purple',
+        title: 'Test badge'
+      },
+      {
+        color: 'blue',
+        title: 'Test badge #2'
+      },
+      {
+        title: 'Test badge #3'
+      }
+    ]
   },
 
   'ivanov': {
