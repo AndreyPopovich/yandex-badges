6841366ddefc1eba16a98b563ecf0904ab3932ef
Andrey Popovich <and.popovich@gmail.com>
2016-07-11 01:21:38 +0300

    Add badges colors

--- data.js
+++ data.js
@@ -6,16 +6,23 @@ module.exports = {
       name: 'Андрей Попович',
       badges: [
         {
+          image: 'images/badges/attention-256.png',
+          title: 'Узнал о Школе разработки интерфейсов'
+        },
+        {
           image: 'images/badges/accept-tick-icon-12.png',
           color: 'purple',
-          title: 'Test badge'
+          title: 'Выполнил задания'
         },
         {
+          image: 'images/badges/flaticon-pencil-2.png',
+          color: 'green',
+          title: 'Заполнил форму регистрации'
+        },
+        {
+          image: 'images/badges/like-flat.png',
           color: 'blue',
-          title: 'Test badge #2'
-        },
-        {
-          title: 'Test badge #3'
+          title: 'Поступил в Школу разработки интерфейсов'
         }
       ]
     },
